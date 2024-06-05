98671a31347589f134b139a60cf20eb80471e1c0
Yuriy Kehter <yuriy.kehter@gmail.com>
2024-06-05 11:09:41 +0400

    Simplify cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -1,9 +1,9 @@
-echo "1) Formatting" && \
-ruff format . && \
-echo "2) Type checking" && \
-mypy . && \
-echo "3) Linting" && \
-ruff check --fix . && \
-echo "4) Testing" && \
-pytest . && \
+echo "1) Formatting"
+ruff format .
+echo "2) Type checking"
+mypy .
+echo "3) Linting"
+ruff check --fix .
+echo "4) Testing"
+pytest .
 echo "Done!"
